a53c79eaff7480f3e3788a8e9b4ea90d0bda6f05
Ace <Temelbahadir1992@gmail.com>
2023-02-27 21:48:43 +0100

    js readability

--- javascript.js
+++ javascript.js
@@ -14,48 +14,63 @@ function getComputerChoice() {
   }
 }
 
-/*
-
-*/
 var playerScore = 0;
 var computerScore = 0;
-
 
 function playRound(playerSelection, computerSelection) {
   switch (playerSelection + computerSelection) {
     case "rockrock":
     case "scissorscissor":
     case "paperpaper":
-      console.log("You both have "+playerSelection+". It's a Draw!");
+      console.log("You both have " + playerSelection + ". It's a Draw!");
       break;
     case "rockscissor":
     case "paperrock":
     case "scissorpaper":
-      console.log("You choose "+playerSelection+" and the Computer choose "+computerSelection+". You Win!");
+      console.log(
+        "You choose " +
+          playerSelection +
+          " and the Computer choose " +
+          computerSelection +
+          ". You Win!"
+      );
       playerScore++;
       break;
     case "scissorrock":
     case "rockpaper":
     case "paperscissor":
-      console.log("You choose "+playerSelection+" and the Computer choose "+computerSelection+". You Lose!");
+      console.log(
+        "You choose " +
+          playerSelection +
+          " and the Computer choose " +
+          computerSelection +
+          ". You Lose!"
+      );
       computerScore++;
       break;
   }
 }
 
-
-function game(){
-  for (; playerScore < 3 && computerScore < 3;){
-    const playerSelection = prompt("Choose between: Rock, Paper and Scissor.").toLowerCase();
+function game() {
+  for (; playerScore < 3 && computerScore < 3; ) {
+    const playerSelection = prompt(
+      "Choose between: Rock, Paper and Scissor."
+    ).toLowerCase();
     const computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
-    console.log("Your Score is "+playerScore+" and the Computers score is "+computerScore+".");
-  }if (playerScore===3) {
-         console.log("You Won the Game!");
+    console.log(
+      "Your Score is " +
+        playerScore +
+        " and the Computers score is " +
+        computerScore +
+        "."
+    );
+  }
+  if (playerScore === 3) {
+    console.log("You Won the Game!");
   } else {
-         console.log("You Lost the Game!");
+    console.log("You Lost the Game!");
   }
 }
-
 
 game();
